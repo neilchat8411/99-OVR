bc4cccaf183e80af5ab4bc961cd9c12bd5e844a7
Your Name <you@example.com>
2026-07-21 09:06:31 -0500

    Accurate 2k ratings

--- teams.js
+++ teams.js
@@ -4,7 +4,7 @@ const teams = [
     name:"Atlanta Hawks",
     logo:"https://cdn.nba.com/logos/nba/1610612737/primary/L/logo.svg",
     ratings:{
-    TIER:"Play-In",
+    TIER:"Playoffs",
     OVR:82,
     INS:75,
     OUT:85,
@@ -123,16 +123,16 @@ const teams = [
     name:"Denver Nuggets",
     logo:"https://cdn.nba.com/logos/nba/1610612743/primary/L/logo.svg",
     ratings:{
-    TIER:"Championship",
+    TIER:"Contender",
     OVR:84,
     INS:77,
     OUT:87,
-    ATH:82,
-    PLA:97,
-    DEF:85,
-    REB:91,
-    INT:94,
-    BEST:97
+    ATH:78,
+    PLA:75,
+    DEF:63,
+    REB:57,
+    INT:79,
+    BEST:98
     }
     },
     
@@ -140,16 +140,16 @@ const teams = [
     name:"Detroit Pistons",
     logo:"https://cdn.nba.com/logos/nba/1610612765/primary/L/logo.svg",
     ratings:{
-    TIER:"Rebuild",
-    OVR:78,
-    INS:82,
-    OUT:77,
-    ATH:84,
-    PLA:80,
+    TIER:"Contender",
+    OVR:83,
+    INS:68,
+    OUT:72,
+    ATH:83,
+    PLA:72,
     DEF:79,
-    REB:83,
-    INT:76,
-    BEST:83
+    REB:60,
+    INT:77,
+    BEST:95
     }
     },
     
@@ -157,16 +157,16 @@ const teams = [
     name:"Golden State Warriors",
     logo:"https://cdn.nba.com/logos/nba/1610612744/primary/L/logo.svg",
     ratings:{
-    TIER:"Playoff",
-    OVR:87,
-    INS:78,
-    OUT:94,
-    ATH:82,
-    PLA:92,
-    DEF:81,
-    REB:78,
-    INT:90,
-    BEST:92
+    TIER:"Play-In",
+    OVR:83,
+    INS:68,
+    OUT:85,
+    ATH:81,
+    PLA:75,
+    DEF:66,
+    REB:55,
+    INT:77,
+    BEST:95
     }
     },
     
@@ -174,16 +174,16 @@ const teams = [
     name:"Houston Rockets",
     logo:"https://cdn.nba.com/logos/nba/1610612745/primary/L/logo.svg",
     ratings:{
-    TIER:"Playoff",
-    OVR:87,
-    INS:88,
-    OUT:80,
-    ATH:91,
-    PLA:84,
-    DEF:89,
-    REB:90,
-    INT:86,
-    BEST:89
+    TIER:"Playoffs",
+    OVR:84,
+    INS:83,
+    OUT:83,
+    ATH:80,
+    PLA:77,
+    DEF:67,
+    REB:54,
+    INT:66,
+    BEST:93
     }
     },
     
@@ -191,16 +191,16 @@ const teams = [
     name:"Indiana Pacers",
     logo:"https://cdn.nba.com/logos/nba/1610612754/primary/L/logo.svg",
     ratings:{
-    TIER:"Playoff",
-    OVR:88,
-    INS:84,
-    OUT:87,
-    ATH:90,
-    PLA:92,
-    DEF:77,
-    REB:81,
-    INT:85,
-    BEST:91
+    TIER:"Playoffs",
+    OVR:84,
+    INS:73,
+    OUT:81,
+    ATH:77,
+    PLA:71,
+    DEF:68,
+    REB:55,
+    INT:77,
+    BEST:93
     }
     },
     
@@ -208,16 +208,16 @@ const teams = [
     name:"Los Angeles Clippers",
     logo:"https://cdn.nba.com/logos/nba/1610612746/primary/L/logo.svg",
     ratings:{
-    TIER:"Contender",
-    OVR:88,
-    INS:87,
-    OUT:88,
-    ATH:85,
-    PLA:86,
-    DEF:90,
-    REB:84,
-    INT:88,
-    BEST:90
+    TIER:"Play-In",
+    OVR:81,
+    INS:63,
+    OUT:86,
+    ATH:79,
+    PLA:75,
+    DEF:52,
+    REB:53,
+    INT:73,
+    BEST:86
     }
     },
     
@@ -226,15 +226,15 @@ const teams = [
     logo:"https://cdn.nba.com/logos/nba/1610612747/primary/L/logo.svg",
     ratings:{
     TIER:"Contender",
-    OVR:90,
-    INS:92,
-    OUT:82,
-    ATH:87,
-    PLA:88,
-    DEF:84,
-    REB:86,
-    INT:91,
-    BEST:96
+    OVR:83,
+    INS:72,
+    OUT:86,
+    ATH:76,
+    PLA:72,
+    DEF:56,
+    REB:54,
+    INT:81,
+    BEST:97
     }
     },
     
@@ -242,16 +242,16 @@ const teams = [
     name:"Memphis Grizzlies",
     logo:"https://cdn.nba.com/logos/nba/1610612763/primary/L/logo.svg",
     ratings:{
-    TIER:"Playoff",
-    OVR:87,
-    INS:86,
-    OUT:79,
-    ATH:92,
-    PLA:83,
-    DEF:93,
-    REB:88,
-    INT:88,
-    BEST:91
+    TIER:"Rebuild",
+    OVR:80,
+    INS:64,
+    OUT:83,
+    ATH:69,
+    PLA:64,
+    DEF:57,
+    REB:56,
+    INT:79,
+    BEST:83
     }
     },
 
@@ -259,16 +259,16 @@ const teams = [
         name:"Miami Heat",
         logo:"https://cdn.nba.com/logos/nba/1610612748/primary/L/logo.svg",
         ratings:{
-        TIER:"Playoff",
-        OVR:86,
-        INS:82,
-        OUT:84,
-        ATH:82,
-        PLA:83,
-        DEF:94,
-        REB:80,
-        INT:92,
-        BEST:89
+        TIER:"Contender",
+        OVR:82,
+        INS:85,
+        OUT:82,
+        ATH:88,
+        PLA:72,
+        DEF:79,
+        REB:59,
+        INT:79,
+        BEST:97
         }
         },
         
@@ -276,16 +276,16 @@ const teams = [
         name:"Milwaukee Bucks",
         logo:"https://cdn.nba.com/logos/nba/1610612749/primary/L/logo.svg",
         ratings:{
-        TIER:"Contender",
-        OVR:90,
-        INS:94,
-        OUT:84,
-        ATH:95,
-        PLA:86,
-        DEF:87,
-        REB:89,
-        INT:89,
-        BEST:96
+        TIER:"Rebuild",
+        OVR:81,
+        INS:59,
+        OUT:85,
+        ATH:77,
+        PLA:79,
+        DEF:59,
+        REB:55,
+        INT:67,
+        BEST:87
         }
         },
         
@@ -294,15 +294,15 @@ const teams = [
         logo:"https://cdn.nba.com/logos/nba/1610612750/primary/L/logo.svg",
         ratings:{
         TIER:"Contender",
-        OVR:91,
-        INS:91,
-        OUT:83,
-        ATH:88,
-        PLA:82,
-        DEF:95,
-        REB:93,
-        INT:90,
-        BEST:95
+        OVR:84,
+        INS:72,
+        OUT:79,
+        ATH:80,
+        PLA:69,
+        DEF:69,
+        REB:51,
+        INT:76,
+        BEST:96
         }
         },
         
@@ -310,16 +310,16 @@ const teams = [
         name:"New Orleans Pelicans",
         logo:"https://cdn.nba.com/logos/nba/1610612740/primary/L/logo.svg",
         ratings:{
-        TIER:"Playoff",
-        OVR:86,
-        INS:90,
-        OUT:82,
-        ATH:94,
-        PLA:84,
-        DEF:86,
-        REB:87,
-        INT:84,
-        BEST:91
+        TIER:"Play-In",
+        OVR:82,
+        INS:76,
+        OUT:80,
+        ATH:80,
+        PLA:67,
+        DEF:57,
+        REB:58,
+        INT:69,
+        BEST:96
         }
         },
         
@@ -327,16 +327,16 @@ const teams = [
         name:"New York Knicks",
         logo:"https://cdn.nba.com/logos/nba/1610612752/primary/L/logo.svg",
         ratings:{
-        TIER:"Contender",
-        OVR:91,
-        INS:89,
-        OUT:84,
-        ATH:85,
-        PLA:81,
-        DEF:92,
-        REB:95,
-        INT:91,
-        BEST:94
+        TIER:"Championship",
+        OVR:84,
+        INS:79,
+        OUT:87,
+        ATH:83,
+        PLA:73,
+        DEF:61,
+        REB:52,
+        INT:74,
+        BEST:96
         }
         },
         
@@ -345,15 +345,15 @@ const teams = [
         logo:"https://cdn.nba.com/logos/nba/1610612760/primary/L/logo.svg",
         ratings:{
         TIER:"Championship",
-        OVR:94,
-        INS:86,
-        OUT:89,
-        ATH:93,
-        PLA:91,
-        DEF:94,
-        REB:85,
-        INT:93,
-        BEST:97
+        OVR:86,
+        INS:79,
+        OUT:86,
+        ATH:81,
+        PLA:76,
+        DEF:78,
+        REB:55,
+        INT:72,
+        BEST:98
         }
         },
         
@@ -361,16 +361,16 @@ const teams = [
         name:"Orlando Magic",
         logo:"https://cdn.nba.com/logos/nba/1610612753/primary/L/logo.svg",
         ratings:{
-        TIER:"Playoff",
-        OVR:86,
-        INS:85,
-        OUT:76,
-        ATH:91,
-        PLA:79,
-        DEF:92,
-        REB:90,
-        INT:87,
-        BEST:89
+        TIER:"Playoffs",
+        OVR:83,
+        INS:80,
+        OUT:84,
+        ATH:79,
+        PLA:75,
+        DEF:59,
+        REB:57,
+        INT:78,
+        BEST:88
         }
         },
         
@@ -378,16 +378,16 @@ const teams = [
         name:"Philadelphia 76ers",
         logo:"https://cdn.nba.com/logos/nba/1610612755/primary/L/logo.svg",
         ratings:{
-        TIER:"Contender",
-        OVR:89,
-        INS:94,
-        OUT:84,
-        ATH:86,
-        PLA:90,
-        DEF:89,
-        REB:91,
-        INT:88,
-        BEST:96
+        TIER:"Championship",
+        OVR:84,
+        INS:74,
+        OUT:90,
+        ATH:81,
+        PLA:77,
+        DEF:69,
+        REB:53,
+        INT:78,
+        BEST:95
         }
         },
         
@@ -395,16 +395,16 @@ const teams = [
         name:"Phoenix Suns",
         logo:"https://cdn.nba.com/logos/nba/1610612756/primary/L/logo.svg",
         ratings:{
-        TIER:"Contender",
-        OVR:88,
-        INS:86,
-        OUT:92,
-        ATH:82,
-        PLA:88,
-        DEF:80,
-        REB:79,
-        INT:88,
-        BEST:94
+        TIER:"Playoffs",
+        OVR:82,
+        INS:71,
+        OUT:82,
+        ATH:80,
+        PLA:62,
+        DEF:79,
+        REB:55,
+        INT:72,
+        BEST:93
         }
         },
         
@@ -412,16 +412,16 @@ const teams = [
         name:"Portland Trail Blazers",
         logo:"https://cdn.nba.com/logos/nba/1610612757/primary/L/logo.svg",
         ratings:{
-        TIER:"Rebuild",
-        OVR:78,
-        INS:80,
-        OUT:81,
-        ATH:85,
-        PLA:83,
-        DEF:77,
-        REB:79,
-        INT:77,
-        BEST:84
+        TIER:"Contender",
+        OVR:84,
+        INS:63,
+        OUT:84,
+        ATH:86,
+        PLA:82,
+        DEF:60,
+        REB:54,
+        INT:70,
+        BEST:88
         }
         },
         
@@ -429,16 +429,16 @@ const teams = [
         name:"Sacramento Kings",
         logo:"https://cdn.nba.com/logos/nba/1610612758/primary/L/logo.svg",
         ratings:{
-        TIER:"Playoff",
-        OVR:86,
-        INS:87,
-        OUT:88,
-        ATH:86,
-        PLA:89,
-        DEF:78,
-        REB:83,
-        INT:84,
-        BEST:90
+        TIER:"Rebuild",
+        OVR:81,
+        INS:79,
+        OUT:83,
+        ATH:74,
+        PLA:63,
+        DEF:50,
+        REB:59,
+        INT:71,
+        BEST:86
         }
         },
         
@@ -446,16 +446,16 @@ const teams = [
         name:"San Antonio Spurs",
         logo:"https://cdn.nba.com/logos/nba/1610612759/primary/L/logo.svg",
         ratings:{
-        TIER:"Rebuild",
-        OVR:82,
-        INS:88,
-        OUT:80,
-        ATH:85,
-        PLA:87,
-        DEF:82,
-        REB:84,
-        INT:86,
-        BEST:94
+        TIER:"Championship",
+        OVR:86,
+        INS:77,
+        OUT:86,
+        ATH:82,
+        PLA:79,
+        DEF:74,
+        REB:57,
+        INT:78,
+        BEST:97
         }
         },
         
@@ -463,16 +463,16 @@ const teams = [
         name:"Toronto Raptors",
         logo:"https://cdn.nba.com/logos/nba/1610612761/primary/L/logo.svg",
         ratings:{
-        TIER:"Rebuild",
-        OVR:81,
+        TIER:"Contender",
+        OVR:84,
         INS:81,
-        OUT:79,
-        ATH:90,
-        PLA:82,
-        DEF:88,
-        REB:86,
-        INT:82,
-        BEST:86
+        OUT:88,
+        ATH:84,
+        PLA:80,
+        DEF:76,
+        REB:57,
+        INT:84,
+        BEST:95
         }
         },
         
@@ -481,15 +481,15 @@ const teams = [
         logo:"https://cdn.nba.com/logos/nba/1610612762/primary/L/logo.svg",
         ratings:{
         TIER:"Rebuild",
-        OVR:80,
-        INS:84,
-        OUT:82,
-        ATH:83,
-        PLA:80,
-        DEF:79,
-        REB:87,
-        INT:80,
-        BEST:85
+        OVR:83,
+        INS:74,
+        OUT:86,
+        ATH:74,
+        PLA:62,
+        DEF:56,
+        REB:57,
+        INT:85,
+        BEST:87
         }
         },
         
@@ -497,16 +497,16 @@ const teams = [
         name:"Washington Wizards",
         logo:"https://cdn.nba.com/logos/nba/1610612764/primary/L/logo.svg",
         ratings:{
-        TIER:"Rebuild",
-        OVR:76,
-        INS:79,
-        OUT:80,
-        ATH:84,
-        PLA:81,
-        DEF:75,
-        REB:78,
-        INT:74,
-        BEST:82
+        TIER:"Play-In",
+        OVR:83,
+        INS:71,
+        OUT:81,
+        ATH:80,
+        PLA:74,
+        DEF:65,
+        REB:58,
+        INT:82,
+        BEST:92
         }
         }
         
